71a6164fa95c858295ddf24142e9f1047d7060c9
Erik Andreazi <andreazi.erik@gmail.com>
2024-03-03 17:55:53 -0300

    Realizando output do target group arn

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -2,6 +2,10 @@ output "sg-id" {
   value = aws_security_group.alb_sg.id
 }
 
+output "tg-arn" {
+  value = aws_lb_target_group.asg-aplicacao.arn
+}
+
 output "alb-endpoint" {
   value = aws_lb.alb-asg-ec2.dns_name
 }
