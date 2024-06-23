f34a3eac7ce8e7bdc8dfea2cb936150b6c1c384f
Erik Andreazi <andreazi.erik@gmail.com>
2024-06-22 21:52:53 -0300

    Iniciando alguns modulos e setando algumas variaveis

--- main.tf
+++ main.tf
@@ -1,10 +1,35 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  name                       = var.projeto
+  projeto                    = var.projeto
   azs                        = var.azs
   vpc_cidr_block             = var.vpc_cidr_block
   public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
   private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
   any-ip                     = var.any-ip
+  gateway_vpc_name           = var.gateway_vpc_name
+}
+
+module "alb" {
+  source = "./modules/alb"
+
+  projeto          = var.projeto
+  vpc_id           = module.vpc.vpc-id
+  public_subnet_id = module.vpc.public_subnet
+}
+
+module "asg-ecs" {
+  source = "./modules/asg-ecs"
+
+  projeto          = var.projeto
+  vpc_id           = module.vpc.vpc-id
+  alb_sg_id        = module.alb.alb_sg_id
+  any_ip           = var.any-ip
+  ecs_ami          = var.ecs_ami
+  lt_instance_type = var.lt_instance_type
+  asg_min_size     = var.asg_min_size
+  asg_max_size     = var.asg_max_size
+  desired_capacity = var.desired_capacity
+  private_subnets  = module.vpc.private_subnet
+  tg_arn           = module.alb.tg_arn
 }
--- variables.tf
+++ variables.tf
@@ -27,3 +27,33 @@ variable "any-ip" {
   type        = string
   description = "IP corringa"
 }
+
+variable "gateway_vpc_name" {
+  type        = string
+  description = "Service name"
+}
+
+variable "ecs_ami" {
+  type        = string
+  description = "Amazon Linux ECS optimized"
+}
+
+variable "lt_instance_type" {
+  type        = string
+  description = "Instance type"
+}
+
+variable "asg_min_size" {
+  type        = string
+  description = "Min EC2 for ASG"
+}
+
+variable "asg_max_size" {
+  type        = string
+  description = "Max EC2 for ASG"
+}
+
+variable "desired_capacity" {
+  type        = string
+  description = "Desired capacity EC2 for ASG"
+}
